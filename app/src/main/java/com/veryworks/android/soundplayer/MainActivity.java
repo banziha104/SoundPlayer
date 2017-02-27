1dfc00fbe419aa4cfd45e8eff908ceba47ca01ba
javafa <nigne2016@gmail.com>
2017-02-27 17:25:21 +0900

    add navigation drawer menu

--- app/src/main/java/com/veryworks/android/soundplayer/MainActivity.java
+++ app/src/main/java/com/veryworks/android/soundplayer/MainActivity.java
@@ -3,7 +3,6 @@ package com.veryworks.android.soundplayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
@@ -14,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.veryworks.android.soundplayer.util.fragment.PagerAdapter;
 
@@ -22,6 +22,8 @@ import static com.veryworks.android.soundplayer.R.id.tab;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity
 
         // 화면의 툴바 가져오기
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // toolbar.setTitleTextColor(Color.BLACK); // 타이틀 텍스트 컬러 변경
         setSupportActionBar(toolbar);
 
         // 플로팅 버튼 설정
@@ -36,8 +39,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                setShuffle();
             }
         });
 
@@ -57,12 +59,12 @@ public class MainActivity extends AppCompatActivity
         // tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE); // 가로축 스크롤하기
 
         // 탭 생성 및 타이틀 입력
-        tabLayout.addTab( tabLayout.newTab().setText("Title") );
-        tabLayout.addTab( tabLayout.newTab().setText("Artist") );
-        tabLayout.addTab( tabLayout.newTab().setText("Album") );
-        tabLayout.addTab( tabLayout.newTab().setText("Genre") );
+        tabLayout.addTab( tabLayout.newTab().setText( getResources().getString(R.string.menu_title)) );
+        tabLayout.addTab( tabLayout.newTab().setText( getResources().getString(R.string.menu_artist)) );
+        tabLayout.addTab( tabLayout.newTab().setText( getResources().getString(R.string.menu_album)) );
+        tabLayout.addTab( tabLayout.newTab().setText( getResources().getString(R.string.menu_genre)) );
         // 2. 뷰페이저
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
         // 아답터 설정 필요
         PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
         // 아답터에 프래그먼트 추가
@@ -81,6 +83,11 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    // 리스트 섞기
+    public void setShuffle(){
+        // TODO 구현합시다
+    }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -90,14 +97,14 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-
+    // 툴바 우측 상단 메뉴 설정
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+    // 툴바 우측 상단 메뉴 onClick
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -106,30 +113,41 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id) {
+            case R.id.action_settings:
+                Toast.makeText(this, "Setting is selected!!!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_mylist:
+                Toast.makeText(this, "My List is selected!!!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_search:
+                Toast.makeText(this, "Search is selected!!!", Toast.LENGTH_SHORT).show();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    // 네비게이션 드로어 메뉴가 onClick 되면 호출
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.menu_title) {
+            viewPager.setCurrentItem(0);
+        } else if (id == R.id.menu_artist) {
+            viewPager.setCurrentItem(1);
+        } else if (id == R.id.menu_album) {
+            viewPager.setCurrentItem(2);
+        } else if (id == R.id.menu_genre) {
+            viewPager.setCurrentItem(3);
+        } else if (id == R.id.nav_mylist) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
